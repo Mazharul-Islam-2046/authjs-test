b17f2617fbcf6e899ce5b30d541723a8d566aae6
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2025-08-21 06:20:06 +0600

    added the prisma and connected it with the mongoDb and perfomed successful login via Google oAuth

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,22 @@
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+import GitHub from "next-auth/providers/github";
+import bcrypt from "bcryptjs";
+import { db } from "./lib/db";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+export const {
+  handlers: { GET, POST },
+  signIn,
+  signOut,
+  auth,
+} = NextAuth({
+  adapter: PrismaAdapter(db),
+  session: {
+    strategy: "jwt",
+  },
+  providers: [Google, GitHub, Credentials({})],
   pages: {
     signIn: "/SignIn",
   },
